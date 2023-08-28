5e5632e311e845389bd4edb9cdaa1e74ab6a8c64
Nidup Dorji <nidupdorji@pop-os.localdomain>
2023-08-28 17:16:21 +0600

    Accepted

---
+++ code1.py
@@ -0,0 +1,6 @@
+def celsius_to_fahrenhiet(celsius):
+    return celsius * 9/5 + 32
+def fahrenhiet_to_celsius(fahrenhiet):
+    return (fahrenhiet - 32) * 5/9
+choice = input("Enter '1' to convert celsius to fahrenhiet, or '2' to convert fahrenhiet to celsius:")
+if choice ==
